c1f97f8d5da4cf734d9a7e9f8b5ac116647f83c4
Ivangelys Salgado <isalgadog@unicartagena.edu.co>
2026-06-01 20:51:52 -0500

    feat: configuracion inicial proyecto hexagonal

---
+++ src/main/java/com/votacion/votacion_hexagonal/VotacionHexagonalApplication.java
@@ -0,0 +1,13 @@
+package com.votacion.votacion_hexagonal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VotacionHexagonalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VotacionHexagonalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/votacion/votacion_hexagonal/VotacionHexagonalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.votacion.votacion_hexagonal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VotacionHexagonalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
